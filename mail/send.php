dabdd8dbe194a418f5e626071d326bba55914ee2
Yusuke <benibana2001@gmail.com>
2018-10-05 05:27:34 +0900

    mb_send

--- mail/send.php
+++ mail/send.php
@@ -1,5 +1,10 @@
 <?php
 
-mail('benibana2001@gmail.com',
+mb_send_mail('benibana2001@gmail.com',
     'test_subject',
-    'test_message');
+    'test_message',
+    $headers);
+
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
