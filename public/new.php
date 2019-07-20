188a4a2618c2354de8eb6033e1b1f7a89bfbddf7
benibana2001 <benibana2001@gmail.com>
2019-07-20 16:23:01 +0900

    add

--- public/new.php
+++ public/new.php
@@ -5,12 +5,33 @@
  * Date: 2019-07-19
  * Time: 02:10
  */
-require_once './bin/GetAllMonth.class.php';
 
+require_once 'bin/vendor/autoload.php';
+
+use Handler\GetAllMonth;
+//use Handler\GetEachMonth;
+
+use WriteData\WriterHTML;
+use WriteData\WriterJson;
+
+// 全てのデータを取得
+// ~/index.php
 $handler = new GetAllMonth();
 
 $data = $handler->getData();
 
+//$writer = new WriterHTML($data);
+$writer = new WriterJson($data);
+
+$writer->write();
+
+//
+// 月別のデータを取得
+// ~/index.php?month=20180801
+
+
+/*
 echo '<pre>';
 var_dump($data);
 echo '</pre>';
+*/
